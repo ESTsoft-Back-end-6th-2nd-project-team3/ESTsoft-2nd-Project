b45ee8ea28c47f4b0ea0966f46f4afcbbd2b3fe5
seunghyun.hwang <dragonray@naver.com>
2024-10-25 17:42:24 +0900

    spring init

---
+++ src/test/java/com/estsoft/estsoft2ndproject/EsTsoft2ndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estsoft.estsoft2ndproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsTsoft2ndProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
