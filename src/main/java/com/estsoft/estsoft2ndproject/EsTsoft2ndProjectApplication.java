266cadf6764396a9112b602ee83d04e21a8e62f4
Jihyun Jeon <hyuneejj@naver.com>
2024-11-04 13:29:12 +0900

    feat: 내 정보 수정 섹션 구현

--- src/main/java/com/estsoft/estsoft2ndproject/EsTsoft2ndProjectApplication.java
+++ src/main/java/com/estsoft/estsoft2ndproject/EsTsoft2ndProjectApplication.java
@@ -2,7 +2,9 @@ package com.estsoft.estsoft2ndproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class EsTsoft2ndProjectApplication {
 
--- src/main/java/com/estsoft/estsoft2ndproject/service/UserService.java
+++ src/main/java/com/estsoft/estsoft2ndproject/service/UserService.java
@@ -1,4 +1,8 @@
 package com.estsoft.estsoft2ndproject.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
 public class UserService {
 }
