8e0764609d7fc5ad7c1dbdd9d0a2c4111580e204
anhyeongmin <alexis0886@naver.com>
2024-11-07 14:44:04 +0900

    상단 메뉴바

--- src/main/java/com/estsoft/estsoft2ndproject/controller/main/PageController.java
+++ src/main/java/com/estsoft/estsoft2ndproject/controller/main/PageController.java
@@ -13,15 +13,26 @@ public class PageController {
 
 	@GetMapping("/index")
 	public String menuPage(Model model) {
-		List<SubMenu> subMenus = Arrays.asList(
-			new SubMenu("카테고리", Arrays.asList("강원도", "경기도", "경상남도", "경상북도", "광주광역시", "대구광역시", "대전광역시", "부산광역시", "서울특별시")),
-			new SubMenu("챌린지", Arrays.asList("챌린지 아이템 1")),
-			new SubMenu("지역 친목 게시판", Arrays.asList("아이템 1", "아이템 2")),
-			new SubMenu("마이페이지", Arrays.asList("프로필 설정", "내 활동 보기"))
-		);
+		List<SubMenu> subMenus;
 
-		// 데이터 확인용 출력
-		System.out.println(subMenus);
+		String level = "관리자";
+
+		// 조건에 따라 서브메뉴 구성
+		if ("관리자".equals(level)) {
+			subMenus = Arrays.asList(
+				new SubMenu("카테고리", Arrays.asList("취미1", "취미2")),
+				new SubMenu("챌린지", Arrays.asList("챌린지 아이템 1", "특별 챌린지")),
+				new SubMenu("관리자 메뉴", Arrays.asList("사용자 관리", "사이트 설정")),
+				new SubMenu("마이페이지", Arrays.asList("프로필 설정", "내 활동 보기", "관리자 전용 항목"))
+			);
+		} else {
+			subMenus = Arrays.asList(
+				new SubMenu("카테고리", Arrays.asList("취미1", "취미2")),
+				new SubMenu("챌린지", Arrays.asList("챌린지 아이템 1")),
+				new SubMenu("지역 친목 게시판", Arrays.asList("강원도", "경기도", "서울특별시")),
+				new SubMenu("마이페이지", Arrays.asList("프로필 설정", "내 활동 보기"))
+			);
+		}
 
 		model.addAttribute("subMenus", subMenus);
 		return "index";
