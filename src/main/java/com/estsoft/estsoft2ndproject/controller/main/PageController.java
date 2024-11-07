03d0d73113a413d1e1d751b65dcd68495acd14ec
anhyeongmin <alexis0886@naver.com>
2024-11-07 15:17:24 +0900

    상단 메뉴바

--- src/main/java/com/estsoft/estsoft2ndproject/controller/main/PageController.java
+++ src/main/java/com/estsoft/estsoft2ndproject/controller/main/PageController.java
@@ -22,8 +22,9 @@ public class PageController {
 			subMenus = Arrays.asList(
 				new SubMenu("카테고리", Arrays.asList("취미1", "취미2")),
 				new SubMenu("챌린지", Arrays.asList("챌린지 아이템 1", "특별 챌린지")),
-				new SubMenu("관리자 메뉴", Arrays.asList("사용자 관리", "사이트 설정")),
-				new SubMenu("마이페이지", Arrays.asList("프로필 설정", "내 활동 보기", "관리자 전용 항목"))
+				new SubMenu("지역 친목 게시판", Arrays.asList("강원도", "경기도", "서울특별시")),
+				new SubMenu("마이페이지", Arrays.asList("프로필 설정", "내 활동 보기")),
+				new SubMenu("관리자 메뉴", Arrays.asList("사용자 관리", "사이트 설정"))
 			);
 		} else {
 			subMenus = Arrays.asList(
