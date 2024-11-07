711d526d961107fb6a5d4d0e4f32c32cda36c3c6
anhyeongmin <alexis0886@naver.com>
2024-11-07 14:24:44 +0900

    상단 메뉴바

--- src/main/java/com/estsoft/estsoft2ndproject/controller/main/PageController.java
+++ src/main/java/com/estsoft/estsoft2ndproject/controller/main/PageController.java
@@ -15,7 +15,7 @@ public class PageController {
 	public String menuPage(Model model) {
 		List<SubMenu> subMenus = Arrays.asList(
 			new SubMenu("카테고리", Arrays.asList("강원도", "경기도", "경상남도", "경상북도", "광주광역시", "대구광역시", "대전광역시", "부산광역시", "서울특별시")),
-			new SubMenu("챌린지", Arrays.asList("챌린지 아이템 1", "챌린지 아이템 2")),
+			new SubMenu("챌린지", Arrays.asList("챌린지 아이템 1")),
 			new SubMenu("지역 친목 게시판", Arrays.asList("아이템 1", "아이템 2")),
 			new SubMenu("마이페이지", Arrays.asList("프로필 설정", "내 활동 보기"))
 		);
