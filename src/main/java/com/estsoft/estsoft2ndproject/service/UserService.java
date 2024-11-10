b9fbde7f1ece12b4f077f55bb4eb57edf4c7dd02
seunghyun.hwang <dragonray@naver.com>
2024-11-10 15:03:32 +0900

    feat: User객체를 보다 쉽게 접글할 수 있도록 수정

--- src/main/java/com/estsoft/estsoft2ndproject/service/UserService.java
+++ src/main/java/com/estsoft/estsoft2ndproject/service/UserService.java
@@ -145,16 +145,4 @@ public class UserService extends DefaultOAuth2UserService {
 
 		userRepository.save(userEntity);
 	}
-
-	public Long getUserId(OAuth2User oAuth2User) {
-		return userRepository.findByPii(oAuth2User.getName()).getUserId();
-	}
-
-	public User getUser(OAuth2User oAuth2User) {
-		return userRepository.findByPii(oAuth2User.getName());
-	}
-
-	public User getUserById(Long userId) {
-		return userRepository.findById(userId).orElse(null);
-	}
 }
