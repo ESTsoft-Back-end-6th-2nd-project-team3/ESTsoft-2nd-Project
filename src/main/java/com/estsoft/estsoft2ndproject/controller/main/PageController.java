e0e763f23f2d3740e415b39847a200984f2d221d
seunghyun.hwang <dragonray@naver.com>
2024-11-10 15:14:19 +0900

    feat: item이 없을 때 하위 메뉴를 표시하지 않게 수정, 관리자 권한일 때 관리자 메뉴를 표시하게 수정

--- src/main/java/com/estsoft/estsoft2ndproject/controller/main/PageController.java
+++ src/main/java/com/estsoft/estsoft2ndproject/controller/main/PageController.java
@@ -1,39 +1,35 @@
 package com.estsoft.estsoft2ndproject.controller.main;
 
 import com.estsoft.estsoft2ndproject.domain.SubMenu;
+import com.estsoft.estsoft2ndproject.domain.User;
+import com.estsoft.estsoft2ndproject.domain.dto.user.CustomUserDetails;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class PageController {
 
-	@GetMapping("/index")
-	public String menuPage(Model model) {
-		List<SubMenu> subMenus;
+	@GetMapping("/")
+	public String menuPage(Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
+		List<SubMenu> subMenus = new ArrayList<>();
+		subMenus.add(new SubMenu("카테고리", Arrays.asList("취미1", "취미2")));
+		subMenus.add(new SubMenu("챌린지", null));
+		subMenus.add(new SubMenu("지역 친목 게시판", Arrays.asList("강원도", "경기도", "서울특별시")));
+		subMenus.add(new SubMenu("마이페이지", Arrays.asList("프로필 설정", "내 활동 보기")));
 
-		String level = "관리자";
+		if (userDetails != null) {
+			User user = userDetails.getUser();
 
-		// 조건에 따라 서브메뉴 구성
-		if ("관리자".equals(level)) {
-			subMenus = Arrays.asList(
-				new SubMenu("카테고리", Arrays.asList("취미1", "취미2")),
-				new SubMenu("챌린지", Arrays.asList("챌린지 아이템 1", "특별 챌린지")),
-				new SubMenu("지역 친목 게시판", Arrays.asList("강원도", "경기도", "서울특별시")),
-				new SubMenu("마이페이지", Arrays.asList("프로필 설정", "내 활동 보기")),
-				new SubMenu("관리자 메뉴", Arrays.asList("사용자 관리", "사이트 설정"))
-			);
-		} else {
-			subMenus = Arrays.asList(
-				new SubMenu("카테고리", Arrays.asList("취미1", "취미2")),
-				new SubMenu("챌린지", Arrays.asList("챌린지 아이템 1")),
-				new SubMenu("지역 친목 게시판", Arrays.asList("강원도", "경기도", "서울특별시")),
-				new SubMenu("마이페이지", Arrays.asList("프로필 설정", "내 활동 보기"))
-			);
+			if (user.getLevel().equals("관리자")) {
+				subMenus.add(3, new SubMenu("관리자 메뉴", null));
+			}
 		}
 
 		model.addAttribute("subMenus", subMenus);
