0988b2a6aa71df61c6cf530e13d45ad1e67bff9b
anhyeongmin <alexis0886@naver.com>
2024-11-11 14:29:17 +0900

    Login UI

--- src/main/java/com/estsoft/estsoft2ndproject/repository/PostRepository.java
+++ src/main/java/com/estsoft/estsoft2ndproject/repository/PostRepository.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.estsoft.estsoft2ndproject.domain.Comment;
 import com.estsoft.estsoft2ndproject.domain.Post;
 import com.estsoft.estsoft2ndproject.domain.dto.admin.PostListResponse;
 
